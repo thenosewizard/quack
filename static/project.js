c0558211d035af225acd8dd18b186b3124332bff
abicharan <abicharan.b@gmail.com>
2018-10-11 15:50:06 +0800

    Added slide bar toggle animation

--- static/project.js
+++ static/project.js
@@ -1,13 +1,18 @@
 // functions to toggle navbar 
 $(document).ready(function () {
     $('#icon').on('click',function () {
-        $('#navOptions').toggle();
+       // $('#navOptions').toggle();
+       $('#navOptions').animate({
+           width: '+=200px'
+       });
     })
 })
 
 $(document).ready(function () {
     $('#cross').on('click',function () {
-        $('#navOptions').toggle();
+       $('#navOptions').animate({
+         width: '0px'
+        })
     })
 })
 
